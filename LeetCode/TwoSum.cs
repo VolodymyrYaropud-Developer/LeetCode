f2ec2c05d426d117fcf1ed5eb6e78227890485fc
Volodymyr Yaropud <bratiskavova@gmail.com>
2024-03-11 19:27:47 +0200

    Add project files.

---
+++ LeetCode/TwoSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    internal static class TwoSum
+    {
+        public static int[] Sum(int[] nums, int target)
+        {
+            int[] res = new int[2];
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        res[0] = i;
+                        res[1] = j;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
